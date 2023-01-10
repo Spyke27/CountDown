56c9bd2c0fa9ae6dbe44510253c8a70c6c063ab2
Rodrigo Daniel <rodrigoeufrasio2@gmail.com>
2023-01-10 20:27:21 -0300

    countdown personalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,13 @@
 import './App.css'
-import Counter from './components/Counter'
-import Title from './components/Title'
 import NewYear from './assets/newyear.jpg'
-import useCountdown from './hooks/useCountdown'
+import { Outlet } from 'react-router-dom'
 
 function App() {
-  const [day, hour, minute, second] = useCountdown("Jan 1, 2024, 00:00:00")
+  
   return (
     <div className="App" style={{backgroundImage:`url(${NewYear})`}}>
       <div className="container">
-        <Title title="Contagem Regressiva para 2024" />
-        <div className="countdown-container">
-        <Counter title="Dias" number={day}/>
-        <Counter title="Horas" number={hour}/>
-        <Counter title="Minutos" number={minute}/>
-        <Counter title="Segundos" number={second}/>
-        </div>
+        <Outlet />
       </div>
     </div>
   )
---
+++ src/routes/Home.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './Home.css'
+
+const Home = () => {
+  return (
+    <div className='home'>
+        <h2>Monte a sua contagem regressiva!</h2>
+        <form className='countdown-form'>
+            <label>
+                <span>Título:</span>
+                <input type="text" name='title' placeholder='Digite o titulo do evento' />
+            </label>
+            <label>
+                <span>Data do Evento:</span>
+                <input type="date" name='date' />
+            </label>
+            <label>
+                <span>Imagem de Fundo:</span>
+                <input type="text" name='image' placeholder='Insira a URL da imagem' />
+            </label>
+            <label>
+                <span>Cor tema:</span>
+                <input type="color" name='color' />
+            </label>
+            <input type="submit" value="Enviar" />
+        </form>
+    </div>
+  )
+}
+
+export default Home
