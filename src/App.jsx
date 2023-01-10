c427eee96aa194140be816907f6e3b06491d5908
Rodrigo Daniel <rodrigoeufrasio2@gmail.com>
2023-01-09 21:01:29 -0300

    contagem regressiva 2024

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,20 @@ import './App.css'
 import Counter from './components/Counter'
 import Title from './components/Title'
 import NewYear from './assets/newyear.jpg'
+import useCountdown from './hooks/useCountdown'
 
 function App() {
+  const [day, hour, minute, second] = useCountdown("Jan 1, 2024, 00:00:00")
   return (
     <div className="App" style={{backgroundImage:`url(${NewYear})`}}>
       <div className="container">
         <Title title="Contagem Regressiva para 2024" />
         <div className="countdown-container">
-        <Counter title="Dias" number={2}/>
-        <Counter title="Horas" number={2}/>
-        <Counter title="Minutos" number={2}/>
-        <Counter title="Segundos" number={2}/>
+        <Counter title="Dias" number={day}/>
+        <Counter title="Horas" number={hour}/>
+        <Counter title="Minutos" number={minute}/>
+        <Counter title="Segundos" number={second}/>
         </div>
-        <p>2 dias</p>
       </div>
     </div>
   )
